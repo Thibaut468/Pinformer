2d94a168eb13f5ed488ff098468b1e06338d2b0a
AdrienL48 <adrien.fortier@insa-lyon.fr>
2019-03-28 17:30:57 +0100

    noms

--- FenetreParametres.java
+++ FenetreParametres.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*; 
 import java.awt.event.*;
-import java.util.ArrayList;
+import java.util.ArrayList;  
 
 	public class FenetreParametres extends JFrame implements ActionListener{
 		
@@ -36,7 +36,8 @@ import java.util.ArrayList;
 			this.setResizable(false); 
 			this.setVisible(false);	
 			
-			Font police = new Font(" Arial ",Font.BOLD,12);
+			Font police1 = new Font(" Arial ",Font.BOLD,9);
+			Font police2 = new Font(" Arial ",Font.BOLD,8);
 
 			String fichier = chargementFichier.chargement("./sauvegardes/param.txt");
 			String[] separation = fichier.split("\\s+");
@@ -53,71 +54,71 @@ import java.util.ArrayList;
 			
 			
 			choix1 = new JButton();
-			choix1.setFont(police);
-			choix1.setText("Choix 1");
-			choix1.setBounds(44,186,80,40);
+			choix1.setFont(police1);
+			choix1.setText("Tchatcheur");
+			choix1.setBounds(34,186,100,40);
 			choix1.setBackground(couleur);
 			choix1.setForeground(Color.white);
 			choix1.setFocusPainted(false);
 			
 			choix2 = new JButton();
-			choix2.setFont(police);
-			choix2.setText("Choix 2");
-			choix2.setBounds(151,186,80,40);
+			choix2.setFont(police1);
+			choix2.setText("CdPoulet");
+			choix2.setBounds(141,186,100,40);
 			choix2.setBackground(couleur);
 			choix2.setForeground(Color.white);
 			choix2.setFocusPainted(false);
 			
 			choix3 = new JButton();
-			choix3.setFont(police);
-			choix3.setText("Choix 3");
-			choix3.setBounds(260,186,80,40);
+			choix3.setFont(police2);
+			choix3.setText("Canard'tichaud");
+			choix3.setBounds(250,186,100,40);
 			choix3.setBackground(couleur);
 			choix3.setForeground(Color.white);
 			choix3.setFocusPainted(false);
 
 			choix4 = new JButton();
-			choix4.setFont(police);
-			choix4.setText("Choix 4");
-			choix4.setBounds(369,186,80,40);
+			choix4.setFont(police1);
+			choix4.setText("Licorne");
+			choix4.setBounds(359,186,100,40);
 			choix4.setBackground(couleur);
 			choix4.setForeground(Color.white);
 			choix4.setFocusPainted(false);
 			
 			choix5 = new JButton();
-			choix5.setFont(police);
-			choix5.setText("Choix 5");
-			choix5.setBounds(44,348,80,40);
+			choix5.setFont(police1);
+			choix5.setText("Tripadvisor");
+			choix5.setBounds(34,348,100,40);
 			choix5.setBackground(couleur);
 			choix5.setForeground(Color.white);
 			choix5.setFocusPainted(false);
 			
 			choix6 = new JButton();
-			choix6.setFont(police);
-			choix6.setText("Choix 6");
-			choix6.setBounds(151,348,80,40);
+			choix6.setFont(police2);
+			choix6.setText("Le Papa Pingouin");
+			choix6.setBounds(141,348,100,40);
 			choix6.setBackground(couleur);
 			choix6.setForeground(Color.white);
 			choix6.setFocusPainted(false);
 			
 			choix7 = new JButton();
-			choix7.setFont(police);
-			choix7.setText("Choix 7");
-			choix7.setBounds(260,348,80,40);
+			choix7.setFont(police1);
+			choix7.setText("Urhino");
+			choix7.setBounds(250,348,100,40);
 			choix7.setBackground(couleur);
 			choix7.setForeground(Color.white);
 			choix7.setFocusPainted(false);
 			
 			choix8 = new JButton();
-			choix8.setFont(police);
-			choix8.setText("Choix 8");
-			choix8.setBounds(369,348,80,40);
+			choix8.setFont(police2);
+			choix8.setText("Morse");
+			choix8.setBounds(359,348,100,40);
 			choix8.setBackground(couleur);
 			choix8.setForeground(Color.white);
 			choix8.setFocusPainted(false);
 			
 			sauvegarde = new JButton();
-			sauvegarde.setFont(police);
+			sauvegarde.setFont(police1);
 			sauvegarde.setText("Sauvegarder");
 			sauvegarde.setBounds(200,420,120,40);
 			sauvegarde.setBackground(couleur);
@@ -128,7 +129,7 @@ import java.util.ArrayList;
 			zqsd = new JRadioButton(text1, unselected);
 			zqsd.setLocation(100,125);
 			zqsd.setSize(100,40);
-			zqsd.setFont(police);
+			zqsd.setFont(police1);
 			zqsd.setBackground(couleur);
 			zqsd.setForeground(Color.white);
 			zqsd.setFocusPainted(false);
@@ -137,7 +138,7 @@ import java.util.ArrayList;
 			fleches = new JRadioButton(text2,selected);
 			fleches.setLocation(300,125);
 			fleches.setSize(100,40);
-			fleches.setFont(police);
+			fleches.setFont(police1);
 			fleches.setBackground(couleur);
 			fleches.setForeground(Color.white);
 			fleches.setFocusPainted(false);
@@ -278,6 +279,6 @@ import java.util.ArrayList;
 			choix6.setBackground(couleur);
 			choix7.setBackground(couleur);
 			choix8.setBackground(couleur);
-		
 	}
+		
 	} 
